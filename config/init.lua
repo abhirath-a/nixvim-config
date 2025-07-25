77bb67a6e207683caab4c52a58d38c9f9a827d3d
Abhi <85126640+abhirath-a@users.noreply.github.com>
2025-07-25 19:35:48 -0400

    transparent bg (for vim-gotham)

--- config/init.lua
+++ config/init.lua
@@ -1,3 +1,7 @@
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'FloatBorder', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'Pmenu', { bg = 'none' })
 vim.cmd.colorscheme("gotham256")
 vim.opt.isfname:append("@-@")
 vim.api.nvim_create_autocmd("LspAttach", {
