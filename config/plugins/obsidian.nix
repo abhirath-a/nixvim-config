d57d35137186ffc88ec220bdc87b788d71045d76
Abhi <85126640+abhirath-a@users.noreply.github.com>
2025-08-07 23:21:28 -0400

    feat(obsidian): added templates folder

--- config/plugins/obsidian.nix
+++ config/plugins/obsidian.nix
@@ -3,13 +3,13 @@
     enable = true;
     settings = {
       completion.blink = true;
-
       workspaces = [
         {
           name = "vault";
           path = "~/vault";
         }
       ];
+      templates.folder = "5 - Templates";
     };
     lazyLoad = {
       enable = true;
